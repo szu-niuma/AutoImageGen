5d3132301c4092ca1ac99ca7fcb0cd0f7efe5bd0
2827709585@qq.com <2827709585@qq.com>
2025-07-02 06:25:51 +0000

    feat: 增加basePipeline的熬出

--- auto_image_edit/pipeline/__init__.py
+++ auto_image_edit/pipeline/__init__.py
@@ -1,3 +1,4 @@
 from .analysis_pipeline import AnalysisPipeline
 from .creativity_pipeline import CreativityPipeline
 from .diff_pipeline import DiffPipeline
+from .base_pipeline import BasePipeline
